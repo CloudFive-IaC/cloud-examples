d54c77add2f0268956644728e5315cb59ebfbcc9
Robert Knoester <info@knoester-it.nl>
2022-06-25 18:40:39 +0200

    changed backend.tf values for demo

--- AzureDevOps-Terraform/terraform/backend.tf
+++ AzureDevOps-Terraform/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "stateterraform184" # Storage account created
-    container_name       = "ct-terraform-state-184" # Container created
+    storage_account_name = "stateterraform" # Replace with Storage account created
+    container_name       = "ct-terraform-state" # Replace with Container created
     key                  = "demo-tf.tfstate" # Desired name of tfstate file
   }
 }
